69c30d23e275317d245185bfc198b520f06ee6c5
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-23 10:06:10 +0600

    read-sync.js

---
+++ nodejs_core_modules/read-sync.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+console.log("Start Reading..........");
+
+try {
+  const data = fs.readFileSync("./data/diary.txt", "utf-8");
+  console.log(data);
+} catch (err) {
+  console.error(err.message);
+}
+
+console.log("Finished");
