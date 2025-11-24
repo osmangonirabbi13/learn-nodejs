7e9716fee056af6f68728ea4f811d7ce4ecb75e7
osmangonirabbi13 <osmangoniyou12@gmail.com>
2025-11-24 11:16:09 +0600

    server create

--- ServerwithNodejs/src/config/index.ts
+++ ServerwithNodejs/src/config/index.ts
@@ -1,4 +1,11 @@
-import  dotenv  from "dotenv";
-import path from "path"
+import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config({path:path.join(process.cwd(), ".env")})
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const config = {
+  env: process.env.NODE_ENV || "development",
+  port: process.env.PORT ? Number(process.env.PORT) : 5000,
+};
+
+export default config;
---
+++ ServerwithNodejs/src/server.ts
@@ -0,0 +1,21 @@
+import http, { Server, ServerResponse, IncomingMessage } from "http";
+import config from "./config";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("Server is running .....");
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello  from node js with typescript",
+          path: req.url,
+        })
+      );
+    }
+  }
+);
+
+server.listen(config.port, () => {
+  console.log(`server is running on port ${config.port}`);
+});
